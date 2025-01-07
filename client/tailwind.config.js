f3c761fd47a72d39dbbd5e616889d10d0a7d91d4
Anil chahar <84167081+Anil12345z@users.noreply.github.com>
2025-01-07 14:36:02 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,25 +1,9 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html", // Include the main HTML file if applicable
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#1d4ed8", // Example custom color
-        secondary: "#9333ea",
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"], // Add a custom font
-      },
-      spacing: {
-        '128': '32rem', // Add custom spacing
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"), // Tailwind forms plugin
-    require("@tailwindcss/typography"), // Tailwind typography plugin
-    require("@tailwindcss/aspect-ratio"), // Tailwind aspect ratio plugin
-  ],
-};
+  plugins: [],
+}
