8f59f4a115c7a72ca245b06c4d3050a847158945
Anil chahar <84167081+Anil12345z@users.noreply.github.com>
2025-01-07 14:31:09 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -13,7 +13,7 @@ export const useSocket = () => {
 export const SocketProvider = (props) => {
   const socket = useMemo(() => {
     const serverURL =
-      process.env.REACT_APP_SOCKET_URL || "http://localhost:8008"; // Fallback to localhost if environment variable is not set
+      process.env.REACT_APP_SOCKET_URL || "http://localhost:8008" || "https://client-4lmh.onrender.com/" ; // Fallback to localhost if environment variable is not set
 
     return io(serverURL, {
       transports: ["websocket", "polling"], // Ensure compatibility across environments
